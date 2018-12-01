4cc59a3961f591f7c7b5d66fa2d4a47478575974
Shadowalker <Shadowalker@192.168.0.14>
2018-12-01 14:48:03 +0100

    Ajouter une fourmi

--- src/entites/Fourmiliere.java
+++ src/entites/Fourmiliere.java
@@ -5,9 +5,25 @@ import java.util.List;
 
 public class Fourmiliere {
 
+  /**
+   * Liste des fourmis qui seront presente dans la formiliere.
+   */
   private List<Fourmi> fourmis;
-  
+
+  /**
+   * Constructeur de la fourmiliere.
+   */
   public Fourmiliere() {
     fourmis = new ArrayList<>();
+  }
+
+  /**
+   * Methode qui permet d'ajouter une fourmi à la fourmiliere.
+   * 
+   * @param fourmi
+   *          la fourmi à ajouter
+   */
+  public void ajouterFourmi(Fourmi fourmi) {
+    fourmis.add(fourmi);
   }
 }
